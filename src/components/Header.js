d0d8ffa1fc395529d44c7c2d937f9236e961d328
Isa Ozler <isa.ozler@frontmen.nl>
2018-10-18 10:29:39 +0200

    func component to class component of the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, Navbar, Alignment } from "@blueprintjs/core";
+import { uniqWith, isEqual, find } from "lodash";
 
 import "../styles/components/Header.scss";
-
-const publicLinks = [
-  {
-    to: "/",
-    text: "Homepage"
-  },
-  {
-    to: "/about",
-    text: "About"
-  },
-  {
-    to: "/profile/1",
-    text: "Profile 1"
-  },
-  {
-    to: "/profile/2",
-    text: "Profile 2"
-  },
-  {
-    to: "/login",
-    text: "Login",
-    auth: false
-  },
-  {
-    to: "/dashboard",
-    text: "Dashboard",
-    auth: true
-  },
-  {
-    to: "/logout",
-    text: "Logout",
-    auth: true
-  },
-  {
-    to: "/this-is-broken",
-    text: "Broken Page"
-  }
-];
-const privateLinks = [
-  {
-    to: "/",
-    text: "Home Page"
-  },
-  {
-    to: "/dashboard",
-    text: "Dashboard",
-    auth: true
-  },
-  {
-    to: "/logout",
-    text: "Logout",
-    auth: true
-  }
-];
 
 const isCurrent = (to, current) => {
   if (to === "/" && current === to) {
@@ -66,25 +13,102 @@ const isCurrent = (to, current) => {
   return false;
 };
 
-export const Header = ({ isAuthenticated, current, history }) => {
-  const HeaderLink = ({ to, text, current }) => (
-    <Button
-      onClick={() => history.push(to)}
-      className={"bp3-minimal " + (isCurrent(to, current) ? "current" : "")}
-      text={text}
-    />
-  );
-  const LoginBtnHandler = () => {
-    history.push("/login");
+export const HeaderLink = ({ to, text, current, history }) => (
+  <Button
+    onClick={() => history.push(to)}
+    className={"bp3-minimal " + (isCurrent(to, current) ? "current" : "")}
+    text={text}
+  />
+);
+
+export class Header extends Component {
+  state = {
+    privateLinks: [
+      {
+        to: "/",
+        text: "Homepage"
+      },
+      {
+        to: "/dashboard",
+        text: "Dashboard",
+        auth: true
+      },
+      {
+        to: "/logout",
+        text: "Logout",
+        auth: true
+      }
+    ],
+    publicLinks: [
+      {
+        to: "/",
+        text: "Homepage"
+      },
+      {
+        to: "/about",
+        text: "About"
+      },
+      {
+        to: "/profile/1",
+        text: "Profile 1"
+      },
+      {
+        to: "/profile/2",
+        text: "Profile 2"
+      },
+      {
+        to: "/login",
+        text: "Login",
+        auth: false
+      },
+      {
+        to: "/dashboard",
+        text: "Dashboard",
+        auth: true
+      },
+      {
+        to: "/logout",
+        text: "Logout",
+        auth: true
+      },
+      {
+        to: "/this-is-broken",
+        text: "Broken Page"
+      }
+    ]
   };
-  const NavItems = () => {
-    let links = isAuthenticated && current !== "/" ? privateLinks : publicLinks;
+
+  isPrivate = current => {
+    const allLinks = uniqWith(
+      [...this.state.privateLinks, ...this.state.publicLinks],
+      isEqual
+    );
+    const link = find(allLinks, { to: current });
+    return link.auth;
+  };
+
+  LoginBtnHandler = () => {
+    this.props.history.push("/login");
+  };
+
+  NavItems = () => {
+    let links =
+      this.props.isAuthenticated && this.isPrivate(this.props.current)
+        ? this.state.privateLinks
+        : this.state.publicLinks;
     return links.map((link, index) => {
-      const NavItem = <HeaderLink key={index} current={current} {...link} />;
+      const NavItem = (
+        <HeaderLink
+          key={index}
+          current={this.props.current}
+          {...link}
+          {...this.props}
+        />
+      );
       if (link.hasOwnProperty("auth")) {
-        if (link.auth && isAuthenticated) {
+        if (link.auth && this.props.isAuthenticated) {
           return NavItem;
-        } else if (!link.auth && !isAuthenticated) {
+        } else if (!link.auth && !this.props.isAuthenticated) {
           return NavItem;
         }
         return null;
@@ -92,22 +116,29 @@ export const Header = ({ isAuthenticated, current, history }) => {
       return NavItem;
     });
   };
-  return (
-    <header id="header">
-      <Navbar>
-        <Navbar.Group align={Alignment.LEFT}>
-          <Navbar.Heading>FrontSlack</Navbar.Heading>
-          <Navbar.Divider />
-          {NavItems()}
-          <Navbar.Divider />
-          <Button
-            intent="success"
-            text={isAuthenticated ? "Go to Dashboard" : "Sign in"}
-            onClick={() => LoginBtnHandler()}
-            className={Alignment.LEFT}
-          />
-        </Navbar.Group>
-      </Navbar>
-    </header>
-  );
-};
+
+  render() {
+    return (
+      <header id="header">
+        <Navbar>
+          <Navbar.Group align={Alignment.LEFT}>
+            <Navbar.Heading>
+              {this.props.isAuthenticated & this.isPrivate(this.props.current)
+                ? "Admin"
+                : "FrontSlack"}
+            </Navbar.Heading>
+            <Navbar.Divider />
+            {this.NavItems()}
+            <Navbar.Divider />
+            <Button
+              intent="success"
+              text={this.props.isAuthenticated ? "Go to Dashboard" : "Sign in"}
+              onClick={() => this.LoginBtnHandler()}
+              className={Alignment.LEFT}
+            />
+          </Navbar.Group>
+        </Navbar>
+      </header>
+    );
+  }
+}
